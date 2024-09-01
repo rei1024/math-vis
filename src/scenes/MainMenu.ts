7a99d8fb3f39dae6b686ab6066e5cae077e9bdcc
rei1024 <satopen1729@gmail.com>
2024-09-01 17:20:28 +0900

    init phaser

---
+++ src/scenes/MainMenu.ts
@@ -0,0 +1,13 @@
+import { Scene } from "phaser";
+
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+  }
+
+  create() {
+    this.input.once("pointerdown", () => {
+      this.scene.start("Game");
+    });
+  }
+}
